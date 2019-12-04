049b19932a02a2d105b823db74bbd14d73a13a6b
Bakui Chou <chou@jsk.imi.i.u-tokyo.ac.jp>
2019-12-04 18:16:45 +0900

    Add topics into rosbag record scripts

--- robots/dragon/bin/rosbag_control_data.sh
+++ robots/dragon/bin/rosbag_control_data.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rosrun aerial_robot_base rosbag_control_data.sh /dragon/joints_ctrl /dragon/joint_states /realsense1/odom/throttle /realsense1/odom
+rosrun aerial_robot_base rosbag_control_data.sh /dragon/joints_ctrl /dragon/joint_states /controller/gimbals_target_force /controller/roll_pitch_gimbal_control /realsense1/odom/throttle /realsense1/odom
