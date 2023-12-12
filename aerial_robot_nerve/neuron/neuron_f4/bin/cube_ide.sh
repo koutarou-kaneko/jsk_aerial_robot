39ba77540356bcab5bd0059b28c08e9175b1f2d9
Kaneko <kaneko@hnl.t.u-tokyo.ac.jp>
2023-12-12 16:20:30 +0900

    [STM32CubeIDE] worlkaround neuron_f4 helped by Junichiro and Kazuki

--- aerial_robot_nerve/neuron/neuron_f4/bin/cube_ide.sh
+++ aerial_robot_nerve/neuron/neuron_f4/bin/cube_ide.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-stm32cubeide $(rospack find spinal)/../neuron/neuron_f4/STM32CubeIDE/.project
+/opt/st/stm32cubeide_1.6.1/stm32cubeide $(rospack find spinal)/../neuron/neuron_f4/STM32CubeIDE/.project
