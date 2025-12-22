1ee32fb509850e6270d9c5375da64490bb73e8e1
Kotaro Kaneko <k.koutarou.0623@gmail.com>
2025-12-22 20:47:43 +0900

    [mthesis] [teleop] change teleop and force feedback to make it enable in any device direction

--- robots/twin_hammer/scripts/teleop_from_mocap_fpv.py
+++ robots/twin_hammer/scripts/teleop_from_mocap_fpv.py
@@ -16,19 +16,34 @@ def exponential(x, base, k_exp):
 def logarithm(x, base, k_log):
   return math.log(x,base) * k_log
 
+def rotate_xy_world_to_body(dx, dy, yaw):
+  c = math.cos(-yaw)
+  s = math.sin(-yaw)
+  bx = c * dx - s * dy
+  by = s * dx + c * dy
+  return bx, by
+  
+def rotate_xy_body_to_world(bx, by, yaw):
+  c = math.cos(yaw)
+  s = math.sin(yaw)
+  wx = c * bx - s * by
+  wy = s * bx + c * by
+  return wx, wy
+
+
 class teleop_from_mocap():
 
   def __init__(self):
 
     self.robot_name = rospy.get_param("~robot_name", "quadrotor")
     self.control_mode = rospy.get_param("~control_mode", "pos") # "pos" or "vel"
-    self.pos_scale = rospy.get_param("~pos_scale", 1.0)
-    self.vel_scale = rospy.get_param("~vel_scale", 0.3)
-    self.ang_vel_scale = rospy.get_param("~ang_vel_scale", 0.08)
+    self.pos_scale = rospy.get_param("~pos_scale", 1.0) # 1.0
+    self.vel_scale = rospy.get_param("~vel_scale", 0.3) # 0.3
+    self.ang_vel_scale = rospy.get_param("~ang_vel_scale", 0.1)
     self.feedback_force_scale = rospy.get_param("~feedback_force_scale", 5.0)
     self.feedback_torque_scale = rospy.get_param("~feedback_torque_scale", 0.8)
     self.velmode_pos_thre = rospy.get_param("~velmode_pos_thre", 0.2)
-    self.velmode_att_thre = rospy.get_param("~velmode_att_thre", 0.2)
+    self.velmode_att_thre = rospy.get_param("~velmode_att_thre", 0.5)
 
     self.nav_pub = rospy.Publisher('/'+self.robot_name+'/uav/nav', FlightNav, queue_size=1)
     # self.att_pub = rospy.Publisher('/'+self.robot_name+'/final_target_baselink_rot', DesireCoord, queue_size=1)
@@ -39,6 +54,7 @@ class teleop_from_mocap():
     self.robot_pos_sub = rospy.Subscriber('/'+self.robot_name+'/mocap/pose', PoseStamped, self.robot_pos_cb)
     self.teleop_mode_sub = rospy.Subscriber('/twin_hammer/teleop_mode', String, self.teleop_mode_cb)
     self.flight_nav = FlightNav()
+    self.flight_nav.control_frame = FlightNav.WORLD_FRAME
     self.flight_nav.target = FlightNav.COG
     self.flight_nav.pos_xy_nav_mode = FlightNav.POS_VEL_MODE
     self.flight_nav.yaw_nav_mode = FlightNav.POS_VEL_MODE
@@ -62,6 +78,8 @@ class teleop_from_mocap():
     self.robot_init_att = [None]*3
     self.robot_vel_mode_fix_pos = [None]*3
     self.robot_vel_mode_fix_att = [None]*3
+    self.robot_local_fix_pos = [None]*3
+    self.robot_local_fix_att = [None]*3
 
     self.device_initialize_flag = False
     self.robot_initialize_flag = False
@@ -92,7 +110,8 @@ class teleop_from_mocap():
       self.robot_initialize_flag = True
 
   def teleop_mode_cb(self,msg):
-    self.initialize_flag = False
+    self.robot_initialize_flag = False
+    self.device_initialize_flag = False
     self.wait_flag = False
     if msg.data == "pos":
       self.control_mode = "pos"
@@ -116,38 +135,65 @@ class teleop_from_mocap():
         self.robot_initialize_flag = False
 
       if self.device_initialize_flag and self.robot_initialize_flag:
+        """ temp hard cording for FPV"""
+        device_dx_world = self.device_pos[0] - self.device_init_pos[0]
+        device_dy_world = self.device_pos[1] - self.device_init_pos[1]
+        device_dz_world = self.device_pos[2] - self.device_init_pos[2]
+        device_dyaw = self.device_att[2] - self.device_init_att[2]
+        device_dx_local, device_dy_local = rotate_xy_world_to_body(
+          device_dx_world, device_dy_world, self.device_init_att[2])
+
+        robot_dx_local = device_dx_local * self.pos_scale
+        robot_dy_local = device_dy_local * self.pos_scale
+        robot_dx_world, robot_dy_world = rotate_xy_body_to_world(
+          robot_dx_local, robot_dy_local, self.robot_init_att[2])
+        target_pos[0] = self.robot_init_pos[0] + robot_dx_world
+        target_pos[1] = self.robot_init_pos[1] + robot_dy_world
+        target_pos[2] = self.robot_init_pos[2] + device_dz_world * self.pos_scale
         for i in range(3):
-          device_pos_diff = self.device_pos[i] - self.device_init_pos[i]
-          device_att_diff = self.device_att[i] - self.device_init_att[i]
-          target_pos[i] = (self.robot_init_pos[i] + device_pos_diff) * self.pos_scale
           target_att[i] = self.device_att[i]
-          target_vel[i] = self.robot_pos[i] + device_pos_diff * self.vel_scale
-          target_ang_vel[i] = self.robot_att[i] + device_att_diff * self.ang_vel_scale
+        target_att[2] = self.robot_init_att[2] + device_dyaw
 
-          """ position fix for vel mode """
-          if abs(device_pos_diff) < self.velmode_pos_thre:
-            if self.robot_vel_mode_fix_pos[i] == None:
-              self.robot_vel_mode_fix_pos[i] = self.robot_pos[i]
-            target_vel[i] = self.robot_vel_mode_fix_pos[i]
-          else:
-            self.robot_vel_mode_fix_pos[i] = None
-          if abs(device_att_diff) < self.velmode_att_thre:
-            if self.robot_vel_mode_fix_att[i] == None:
-              self.robot_vel_mode_fix_att[i] = self.robot_att[i]
-            target_ang_vel[i] = self.robot_vel_mode_fix_att[i]
-          else:
-            self.robot_vel_mode_fix_att[i] = None
-
-          """ convert feedback wrench with log """
-          if device_pos_diff >= 0:
-            feedback_wrench[i] = -logarithm(device_pos_diff+1, log_base, self.feedback_force_scale)
-          else:
-            feedback_wrench[i] = logarithm(-device_pos_diff+1, log_base, self.feedback_force_scale)
-          if device_att_diff >= 0:
-            feedback_wrench[i+3] = -logarithm(device_att_diff+1, log_base, self.feedback_torque_scale)
-          else:
-            feedback_wrench[i+3] = logarithm(-device_att_diff+1, log_base, self.feedback_torque_scale)
+        robot_vx_local = device_dx_local * self.vel_scale
+        robot_vy_local = device_dy_local * self.vel_scale
+        robot_vyaw = device_dyaw * self.ang_vel_scale
         
+        if abs(device_dx_local) < self.velmode_pos_thre:
+          robot_vx_local = 0.0
+        if abs(device_dy_local) < self.velmode_pos_thre:
+          robot_vy_local = 0.0
+          
+        if abs(device_dyaw) < self.velmode_att_thre:
+          if self.robot_local_fix_att[2] == None:
+            self.robot_local_fix_att[2] = self.robot_att[2]
+          target_ang_vel[2] = self.robot_local_fix_att[2]
+        else:
+          self.robot_local_fix_att[2] = None
+          target_ang_vel[2] = self.robot_att[2] + robot_vyaw
+
+        robot_vx_world, robot_vy_world = rotate_xy_body_to_world(
+          robot_vx_local, robot_vy_local, self.robot_att[2])
+        target_vel[0] = self.robot_pos[0] + robot_vx_world
+        target_vel[1] = self.robot_pos[1] + robot_vy_world
+
+        if device_dx_local >= 0:
+          feedback_wrench_x_local = -logarithm(device_dx_local+1, log_base, self.feedback_force_scale)
+        else:
+          feedback_wrench_x_local = logarithm(-device_dx_local+1, log_base, self.feedback_force_scale)
+        if device_dy_local >= 0:
+          feedback_wrench_y_local = -logarithm(device_dy_local+1, log_base, self.feedback_force_scale)
+        else:
+          feedback_wrench_y_local = logarithm(-device_dy_local+1, log_base, self.feedback_force_scale)
+        if device_dyaw >= 0:
+          feedback_wrench_yaw = -logarithm(device_dyaw+1, log_base, self.feedback_torque_scale)
+        else:
+          feedback_wrench_yaw = logarithm(-device_dyaw+1, log_base, self.feedback_torque_scale)
+        feedback_wrench[0], feedback_wrench[1] = rotate_xy_body_to_world(
+          feedback_wrench_x_local, feedback_wrench_y_local, self.device_att[2])
+        feedback_wrench[5] = feedback_wrench_yaw
+
+
+
         """ limitation of z and att for safety """
         if self.robot_pos[2] > 1.2:
           target_pos[2] = 1.2
@@ -155,7 +201,7 @@ class teleop_from_mocap():
         if self.robot_pos[2] < 0.3:
           target_pos[2] = 0.3
           target_vel[2] = 0.0
-        limit_angle = 0.35
+        limit_angle = 0.2
         for i in range(2):  # except yaw
           if target_att[i] > limit_angle:
             target_att[i] = limit_angle
@@ -165,6 +211,17 @@ class teleop_from_mocap():
             target_ang_vel[i] = limit_angle
           if target_ang_vel[i] < -limit_angle:
             target_ang_vel[i] = -limit_angle
+        force_limit = 15
+        torque_limit = 2.0
+        for i in range(3):
+          if feedback_wrench[i] > force_limit:
+            feedback_wrench[i] = force_limit
+          if feedback_wrench[i] < -force_limit:
+            feedback_wrench[i] = -force_limit
+          if feedback_wrench[i+3] > torque_limit:
+            feedback_wrench[i+3] = torque_limit
+          if feedback_wrench[i+3] < -torque_limit:
+            feedback_wrench[i+3] = -torque_limit
 
         if self.control_mode == "pos":
           self.flight_nav.target_pos_x = target_pos[0]
