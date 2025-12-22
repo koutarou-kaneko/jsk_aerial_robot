5d8ceab7afbefca520f851e1a53cb0f2a53914b6
Kotaro Kaneko <k.koutarou.0623@gmail.com>
2025-12-22 20:47:43 +0900

    [mthesis] [teleop] change teleop and force feedback to make it enable in any device direction

---
+++ robots/twin_hammer/scripts/force_feedback_from_robot_fpv.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+
+import rospy
+import time
+import math
+import numpy as np
+import tf.transformations as tf
+from scipy.spatial.transform import Rotation as R
+from std_msgs.msg import Int8
+from aerial_robot_msgs.msg import FlightNav
+from geometry_msgs.msg import PoseStamped, WrenchStamped
+
+def exponential(x, base, k_exp):
+  return pow(x,base) * k_exp
+
+def logarithm(x, base, k_log):
+  return math.log(x,base) * k_log
+
+def rotate_xy_world_to_body(dx, dy, yaw):
+  c = math.cos(yaw)
+  s = math.sin(yaw)
+  bx = c * dx - s * dy
+  by = s * dx + c * dy
+  return bx, by
+
+class force_feedback_from_robot():
+
+  def __init__(self):
+
+    self.robot_name = rospy.get_param("~robot_name", "gimbalrotor")
+    self.convert_method = rospy.get_param("~convert_method", "prop") # "prop" or "exp" or "log"
+    self.feedback_from_ang = rospy.get_param("~feedback_from_ang", "False")
+
+    # self.haptics_switch_pub = rospy.Publisher('/twin_hammer/haptics_switch', Int8, queue_size=1)
+    self.haptics_wrench_pub = rospy.Publisher('/twin_hammer/haptics_wrench', WrenchStamped, queue_size=1)
+    self.robot_mocap_sub = rospy.Subscriber('/'+self.robot_name+'/mocap/pose', PoseStamped, self.robot_mocap_cb)
+    self.device_mocap_sub = rospy.Subscriber('/twin_hammer/mocap/pose', PoseStamped, self.device_mocap_cb)
+    # self.robot_wrench_sub = rospy.Subscriber('/cfs/data', WrenchStamped, self.robot_wrench_cb)
+    self.robot_wrench_sub = rospy.Subscriber('/filtered_ftsensor', WrenchStamped, self.robot_wrench_cb)
+
+    self.haptics_wrench_msg = WrenchStamped()
+    self.robot_wrench = []
+    self.filterd_robot_wrench_local = [0.0,0.0,0.0,0.0,0.0,0.0]
+    self.robot_att = [0.0,0.0,0.0]
+    self.device_att = [0.0,0.0,0.0]
+    self.device_init_yaw = None
+    self.k_att_diff = 1.0
+    self.Ad_R_robot = np.block([
+      [np.identity(3), np.zeros((3,3))],
+      [np.zeros((3,3)), np.identity(3)]
+    ])
+    self.Ad_R_inv_device = np.block([
+      [np.identity(3), np.zeros((3,3))],
+      [np.zeros((3,3)), np.identity(3)]
+    ])
+    self.moment_arm = np.array([-(0.044 + 0.025), 0, 0])
+    self.k_p = 2.0
+    self.exp_base = 1.45
+    self.log_base = 1.45
+    self.k_exp = 0.4
+    self.k_log = 1.0
+    """ belows are dependent valuables """
+    self.range_log = math.e
+    self.a_log = self.k_log / (math.e * math.log(self.log_base))
+    # time.sleep(0.5)
+
+  def robot_mocap_cb(self,msg):
+    # self.robot_att = [msg.pose.orientation.roll, msg.pose.orientation.pitch, msg.pose.orientation.yaw] 
+    q = [msg.pose.orientation.x,msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w]
+    rot = R.from_quat(q)
+    self.robot_att = rot.as_euler('xyz', degrees=False)
+    R_mat = rot.as_matrix()
+    self.Ad_R_robot = np.block([
+    [R_mat, np.zeros((3, 3))],
+    [np.zeros((3, 3)), R_mat]
+    ])
+
+  def device_mocap_cb(self,msg):
+    # self.device_att = [msg.pose.orientation.roll, msg.pose.orientation.pitch, msg.pose.orientation.yaw] 
+    q = [msg.pose.orientation.x,msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w]
+    rot = R.from_quat(q)
+    self.device_att = rot.as_euler('xyz', degrees=False)
+    if self.device_init_yaw == None:
+      self.device_init_yaw = self.device_att[2]
+    R_mat = rot.as_matrix()
+    self.Ad_R_inv_device = np.block([
+    [R_mat.T, np.zeros((3, 3))],
+    [np.zeros((3, 3)), R_mat.T]
+    ])
+
+  def robot_wrench_cb(self,msg):
+    """ below mapping is depend on the direction for installing force sensor """
+    force_x = msg.wrench.force.z
+    force_y = msg.wrench.force.x
+    force_z = msg.wrench.force.y
+    torque_x = msg.wrench.torque.z
+    torque_y = msg.wrench.torque.x
+    torque_z = msg.wrench.torque.y
+
+    force_np = np.array([force_x,force_y,force_z])
+    torque_from_force = np.cross(force_np,self.moment_arm)
+    wrench_local = [force_x, force_y, force_z, torque_x, torque_y, torque_z]
+    delay_param = 0.05
+    for i in range (len(wrench_local)):
+      self.filterd_robot_wrench_local[i] = (1-delay_param) * self.filterd_robot_wrench_local[i] + delay_param * wrench_local[i]
+    self.robot_wrench = self.filterd_robot_wrench_local
+
+  def main(self):
+    r = rospy.Rate(40)
+    while not rospy.is_shutdown():
+      haptics_wrench = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+      for i in range(len(self.robot_wrench)):
+        wrench_i = self.robot_wrench[i]
+
+        """ propotional conversion """
+        if self.convert_method == "prop":
+          haptics_wrench[i] = wrench_i * self.k_p
+
+        """ exponential conversion """
+        if self.convert_method == "exp":
+          if wrench_i>=0.0:
+            haptics_wrench[i] = exponential(wrench_i, self.exp_base, self.k_exp)
+          else:
+            haptics_wrench[i] = -exponential(-wrench_i, self.exp_base, self.k_exp)
+
+        """ log conversion """
+        if self.convert_method == "log":
+          if wrench_i>=0:
+            haptics_wrench[i] = logarithm(wrench_i+1, self.log_base, self.k_log)
+          elif wrench_i<-self.range_log:
+            haptics_wrench[i] = -logarithm(-(wrench_i-1), self.log_base, self.k_log)
+
+      if self.device_init_yaw != None:
+        wrench_device_local_x, wrench_device_local_y = rotate_xy_world_to_body(
+          haptics_wrench[0], haptics_wrench[1], self.device_init_yaw)
+        haptics_wrench[0] = wrench_device_local_x
+        haptics_wrench[1] = wrench_device_local_y   
+
+        force_limit = 10
+        torque_limit = 2
+        for i in range(3):
+          if haptics_wrench[i] > force_limit:
+            haptics_wrench[i] = force_limit
+          if haptics_wrench[i] < -force_limit:
+            haptics_wrench[i] = -force_limit
+          if haptics_wrench[i+3] > torque_limit:
+            haptics_wrench[i+3] = torque_limit
+          if haptics_wrench[i+3] < -torque_limit:
+            haptics_wrench[i+3] = -torque_limit
+
+        self.haptics_wrench_msg.wrench.force.x = haptics_wrench[0]
+        self.haptics_wrench_msg.wrench.force.y = haptics_wrench[1]
+        self.haptics_wrench_msg.wrench.force.z = haptics_wrench[2]
+        self.haptics_wrench_msg.wrench.torque.x = haptics_wrench[3]
+        self.haptics_wrench_msg.wrench.torque.y = haptics_wrench[4]
+        self.haptics_wrench_msg.wrench.torque.z = haptics_wrench[5]
+        self.haptics_wrench_pub.publish(self.haptics_wrench_msg)
+
+      r.sleep()
+
+if __name__ == "__main__":
+  rospy.init_node("force_feedback_from_robot")
+  Tracker = force_feedback_from_robot()
+  Tracker.main()
